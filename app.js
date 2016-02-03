f359fc4fbd8fd7f2f0db7ea14f747b1576dc931f
Yongliang Zhan <yzhan94@gmail.com>
2016-02-03 11:15:50 +0100

    Send REST requests to mock database
    Synchronization approach doesn't work really well.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 var Firebase = require('firebase');
 var FirebaseTokenGenerator = require('firebase-token-generator');
+var https = require('https');
+var querystring = require('querystring');
 
 /**
 ** Item definition
@@ -33,24 +35,55 @@ function setSync(ref, val) {
 	});
 }
 
+function getOptions(method, itemId) {
+	var options = {
+		hostname: 'brilliant-torch-8285.firebaseio.com',
+		path: '/lr-mockdb/items/' + itemId + '.json',
+		method: method,
+	};
+	return options;
+}
+
 itemRef.on('child_added', function(snapshot) {
 	if (!snapshot.val().sync) {
-		setSync(itemRef.child(snapshot.key()), true);
-		console.log('item added: ' + snapshot.key());
+		var options = getOptions('PUT', snapshot.key());
+		var req = https.request(options, function(response) {
+			if (response.statusCode == 200) {
+				setSync(itemRef.child(snapshot.key()), true);
+				console.log('item added: ' + snapshot.key());
+			}
+		});
+
+		var data = JSON.stringify(snapshot.val());
+		req.write(data);
+		req.end();
 	}
 });
 
 itemRef.on('child_removed', function(snapshot) {
 	if (!snapshot.val().sync) {
-
-		console.log('item removed: ' + snapshot.key());
+		var options = getOptions('DELETE', snapshot.key());
+		var req = https.request(options, function(response) {
+			if (response.statusCode == 200) {
+				console.log('item removed: ' + snapshot.key());
+			}
+		});
+		req.end();
 	}
 });
 
 itemRef.on('child_changed', function(snapshot) {
 	if (!snapshot.val().sync) {
-		setSync(itemRef.child(snapshot.key()), true);
-		console.log('item changed: ' + snapshot.key());
+		var options = getOptions('PATCH', snapshot.key());
+		var req = https.request(options, function(response) {
+			if (response.statusCode == 200) {
+				setSync(itemRef.child(snapshot.key()), true);
+				console.log('item changed: ' + snapshot.key());
+			}
+		});
+		var data = JSON.stringify(snapshot.val());
+		req.write(data);
+		req.end();
 	}
 });
 
